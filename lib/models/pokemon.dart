25032bbbab0f0de761d0bdf15cc0ecd1ac4ef6f0
Alan Nadolny <alannadolnyy@gmail.com>
2024-07-24 01:10:45 +0200

    Move some logic to pokemon data model and add new package for getting color palette

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,22 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
+
 class Pokemon {
+  int? id;
   String name;
   String? url;
-  String? image;
-  int? id;
+  String? officialArtworkImage;
 
   Pokemon({
     required this.name,
-    this.url,
-    this.image,
     this.id,
+    this.url,
+    this.officialArtworkImage,
   });
+
+  String get displayId {
+    return id.toString().padLeft(3, '0');
+  }
+
+  String get displayName {
+    return name[0].toUpperCase() + name.substring(1).toLowerCase();
+  }
+
+  Future<PaletteGenerator> get colorPalette async {
+    final PaletteGenerator paletteGenerator =
+        await PaletteGenerator.fromImageProvider(
+      Image.network(officialArtworkImage!).image,
+    );
+
+    return paletteGenerator;
+  }
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
     return Pokemon(
-        name  : json['name'],
-        image : json['image'],
-        url   : json['url'],
-        id    : json['id'],
+      id: json['id'],
+      name: json['name'],
+      url: json['url'],
+      officialArtworkImage: json['sprites']?['other']?['official-artwork']
+          ?['front_default'],
     );
   }
 }
