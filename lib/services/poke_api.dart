f532ca75a659ce47ee848d709119db15467cad8c
Alan Nadolny <alannadolnyy@gmail.com>
2024-07-23 23:36:29 +0200

    Add service for fetching pokemons details

---
+++ lib/services/poke_api.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pokedex_app/models/pokemon.dart';
+
+class PokeApi {
+
+  static Future<List<Pokemon>> fetchPokemons() async {
+    final response =
+    await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon'));
+
+    if (response.statusCode == 200) {
+      return List.of(json.decode(response.body)['results'])
+          .map((value) => Pokemon.fromJson(value))
+          .toList();
+    }
+
+    throw Exception('Failed to fetch pokemons');
+  }
+
+  static Future<Pokemon> fetchPokemonDetails(String url) async {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return Pokemon.fromJson(json.decode(response.body));
+    }
+
+    throw Exception('Failed to fetch pokemon');
+  }
+  
+  static Future<List<Pokemon>> fetchPokemonsWithDetails() async {
+    final List<Pokemon> pokemonsList = await fetchPokemons();
+    List<Future<Pokemon>> pokemonsWithDetails = pokemonsList
+        .map((pokemon) => fetchPokemonDetails(pokemon.url!))
+        .toList();
+
+    return Future.wait(pokemonsWithDetails);
+  }
+}
